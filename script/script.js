6c981ab2ff2c0d5204be080b803b2a37b666c1d6
Roman E <warrior.394@yandex.ru>
2022-02-17 23:54:17 +0300

    исправления

--- script/script.js
+++ script/script.js
@@ -1,47 +1,35 @@
-const ProfileEditButton = document.querySelector('.profile__edit-button');
+const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
-const formElement = document.querySelector('.popup__form-info');
-const popupSubmitButton = document.querySelector('.popup__submit-button');
-const inputName = document.querySelector('.popup__input-name');
-const inputInfo = document.querySelector('.popup__input-info');
+const formElement = document.querySelector('.popup__input-container');
+const inputName = document.querySelector('.popup_input-name');
+const inputInfo = document.querySelector('.popup_input-info');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
-const popupWindow = document.querySelector('.popup_type_edit-profile');
-const elements = document.querySelector('.card');
-const card = document.querySelector('.card__element');
-
+const popupTypeEditProfile = document.querySelector('.popup_type_edit-profile');
 
 
 function openPopup() {
-    popup.classList.add('popup__opened');
+    popup.classList.add('popup_opened');
 }
-
-ProfileEditButton.addEventListener('click', openPopup);
 
 function closePopup() {
-    popup.classList.remove('popup__opened');
+    popup.classList.remove('popup_opened');
 }
 
-popupCloseButton.addEventListener('click', closePopup);
-
 function popupOpenForm() {
-    openPopup(popupWindow);
+    openPopup(popupTypeEditProfile)
+    profileName.value = profileDescription.textContent;
     inputName.value = profileName.textContent;
-    inputInfo.value = profileDescription.textContent;
 }
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileDescription.textContent = inputInfo.value;
-    closePopup(popupWindow);
+    closePopup(popupTypeEditProfile)
 }
 
-formElement.addEventListener('submit', formSubmitHandler); 
-
-const likeButton = card.querySelector('.card__like');
-
-likeButton.addEventListener('click', () => {
-    likeButton.classList.toggle('card__like_clicked');
-})
+formElement.addEventListener('submit', formSubmitHandler);
+profileEditButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
