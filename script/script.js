24aa2e6446df4d63b4c8006563216bf977e8bd23
Roman E <warrior.394@yandex.ru>
2022-02-21 00:14:30 +0300

    class

--- script/script.js
+++ script/script.js
@@ -2,8 +2,8 @@ const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const formElement = document.querySelector('.popup__input-container');
-const inputName = document.querySelector('.popup_input_type_name');
-const inputInfo = document.querySelector('.popup_input_type_info');
+const inputName = document.querySelector('.popup__input_type_name');
+const inputInfo = document.querySelector('.popup__input_type_info');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 
