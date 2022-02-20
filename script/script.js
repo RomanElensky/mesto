d1065f2d5c1f005de6292cb75d75c50becc01ecc
Roman E <warrior.394@yandex.ru>
2022-02-20 23:01:34 +0300

    исправления

--- script/script.js
+++ script/script.js
@@ -2,11 +2,11 @@ const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const formElement = document.querySelector('.popup__input-container');
-const inputName = document.querySelector('.popup__input-name');
-const inputInfo = document.querySelector('.popup__input-info');
+const inputName = document.querySelector('.popup_input_type_name');
+const inputInfo = document.querySelector('.popup_input_type_info');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
-const popupTypeEditProfile = document.querySelector('.popup_type_edit-profile');
+
 
 
 function openPopup() {
@@ -18,7 +18,7 @@ function closePopup() {
 }
 
 function popupOpenForm() {
-    openPopup(popupTypeEditProfile)
+    openPopup()
     inputName.value = profileName.textContent;
     inputInfo.value = profileDescription.textContent;
 }
@@ -27,7 +27,7 @@ function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileDescription.textContent = inputInfo.value;
-    closePopup(popupTypeEditProfile)
+    closePopup()
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
