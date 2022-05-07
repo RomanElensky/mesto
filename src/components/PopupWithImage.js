5c01e3c9d25f63381e119729dbcba9c71681c0f1
Roman Elensky <warrior.394@yandex.ru>
2022-05-07 18:05:16 +0400

    5 2

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,8 +3,8 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
     constructor(popupSelector) {
         super(popupSelector);
-        this._popupImage = document.querySelector('.popup__image'); 
-        this._popupTitle = document.querySelector('.popup__image-title'); 
+        this._popupImage = this._popupElement.querySelector('.popup__image'); 
+        this._popupTitle = this._popupElement.querySelector('.popup__image-title');
     }
 
     open(name, link) {
